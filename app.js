139e4e43c2482cad49ca7f57a601684e7afe3b84
dhanu <dhanusankar.m@gmail.com>
2024-03-30 18:27:02 -0500

    file updated and html created

--- app.js
+++ app.js
@@ -76,10 +76,6 @@ app.get('/', (req, res) => {
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route to serve your HTML page
-//app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-//});
 
 app.post('/execute', async (req, res) => {
     const searchTerm = req.query.searchTerm;
